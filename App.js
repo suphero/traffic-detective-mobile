ac5416a0705752fd94c269021138ca1ff75a04fb
suphero <harunsokullu@gmail.com>
2018-12-08 14:04:21 +0300

    add apollo-link-error

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-client-preset'
+import { onError } from "apollo-link-error";
 import Root from './Root'
 
 const httpLink = new HttpLink({ uri: 'http://192.168.1.244:3000/graphql' })
+const errorLink = onError(({ networkError }) => {
+  // if (networkError.statusCode === 401) {
+  //   logout();
+  // }
+  console.log(networkError);
+});
+const link = errorLink.concat(httpLink);
 
 const client = new ApolloClient({
-  link: httpLink,
+  link,
   cache: new InMemoryCache(),
 })
 
